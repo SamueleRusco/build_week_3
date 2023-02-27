3bd31d157e5b35be0f62f054e27dfd0883a4378e
AleBellEpicode <114953308+AleBellEpicode@users.noreply.github.com>
2023-02-27 18:40:12 +0100

    ultimo commit. put non completa

--- src/Components/MyForm.jsx
+++ src/Components/MyForm.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
@@ -11,16 +10,46 @@ const MyForm = () => {
   const [bio, setBio] = useState("");
   const [area, setArea] = useState("");
 
-  const putProfileFetch = () => {};
-
-  useEffect(() => {
-    putProfileFetch();
-  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     putProfileFetch();
   };
 
+  const baseEndpoint = "https://striveschool-api.herokuapp.com/api/profile/";
+  /*  let userId = "63fc659cf193e60013807f4d"; */
+  /* function putProfileFetch() {
+    let key =
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNjU5Y2YxOTNlNjAwMTM4MDdmNGQiLCJpYXQiOjE2Nzc0ODU0NzMsImV4cCI6MTY3ODY5NTA3M30.4UuEx0E0rg5moiQl2yjBzNkAo75xaKrDS6hY-r_GSLI";
+    // Send data to the backend via POST
+    return async () => {
+      const response = await fetch(baseEndpoint, {
+        method: "PUT",
+        headers: { authorization: key },
+        body: JSON.stringify({
+          name: name,
+          surname: surname,
+          email: email,
+          username: username,
+          title: title,
+          bio: bio,
+          area: area,
+        }),
+      });
+      console.log("sono il put", response);
+    };
+  } */
+
+  const putProfileFetch = async () => {
+    let user = { name, surname, email, username, title, bio, area };
+    let key =
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNjU5Y2YxOTNlNjAwMTM4MDdmNGQiLCJpYXQiOjE2Nzc0ODU0NzMsImV4cCI6MTY3ODY5NTA3M30.4UuEx0E0rg5moiQl2yjBzNkAo75xaKrDS6hY-r_GSLI";
+    await fetch(baseEndpoint, {
+      method: "PUT",
+      headers: { authorization: key },
+      body: JSON.stringify(user),
+    });
+    console.log("sono put", user);
+  };
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -43,7 +72,7 @@ const MyForm = () => {
         <Form.Control
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          type="email"
+          type="input"
           placeholder="Enter email"
         />
         <Form.Label>Username</Form.Label>
