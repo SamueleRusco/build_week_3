edb7798d2417e10e76eea3274b1484fcd6477180
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-02-27 17:10:41 +0100

    fwdf

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import MyNavBarComponents from "./Components/MyNavBarComponents";
+import MyFooterPart from "./Components/MyFooterPart";
+
 
 function App() {
   return (
     <div className="App">
       <MyNavBarComponents/>
+      <MyFooterPart/>
     </div>
   );
 }
