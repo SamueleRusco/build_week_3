bfa70301427ff77e0bbc47cb9ab235973e72c4b1
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-02-28 10:01:08 +0100

    Commit

--- src/Components/MyprofileSettingsCard.jsx
+++ src/Components/MyprofileSettingsCard.jsx
@@ -1,5 +1,10 @@
 import { Card, Row, Col, Button } from "react-bootstrap";
-import { EyeFill, Pencil, PlusLg } from "react-bootstrap-icons";
+import {
+  EyeFill,
+  Pencil,
+  PlusLg,
+  QuestionCircleFill,
+} from "react-bootstrap-icons";
 import { useState } from "react";
 
 const MyProfileSettongsComponent = () => {
@@ -12,48 +17,55 @@ const MyProfileSettongsComponent = () => {
             className=" position-absolute bg-transparent p-0"
             style={{
               border: "none",
-              top: "25%",
-              right: "13%",
-              fontSize: "1.5rem",
+              top: "16%",
+              right: "9%",
+              fontSize: "1rem",
             }}
           >
-            <PlusLg className="text-dark" />
+            <QuestionCircleFill style={{ color: "grey" }} />
+          </Button>
+          <Button
+            className=" position-absolute bg-transparent p-0"
+            style={{
+              border: "none",
+              top: "60%",
+              right: "9%",
+              fontSize: "1rem",
+            }}
+          >
+            <QuestionCircleFill style={{ color: "grey" }} />
           </Button>
 
           <Row>
-            <Col xs={11}>
+            <Col
+              xs={11}
+              className="pb-3"
+              style={{ borderBottom: "1px solid grey" }}
+            >
               <Card.Title
                 className="mb-0"
                 style={{
-                  fontSize: "1.1rem",
+                  fontSize: "1rem",
                   alignSelf: "center",
                   color: "grey",
                 }}
               >
-                {"Modifica il profilo pubblico e l'url"}
+                {"Modifica il profilo pubblico e l'URL"}
               </Card.Title>
-              <Col xs={11}>
-                <Card.Title
-                  className="mb-0"
-                  style={{
-                    fontSize: "1.1rem",
-                    alignSelf: "center",
-                    color: "grey",
-                  }}
-                >
-                  {"Aggiungi il tuo profilo in un altra lingua"}
-                </Card.Title>
-              </Col>
+            </Col>
+            <Col xs={11} className="pt-3">
+              <Card.Title
+                className="mb-0"
+                style={{
+                  fontSize: "1rem",
+                  alignSelf: "center",
+                  color: "grey",
+                }}
+              >
+                {"Aggiungi il tuo profilo in un altra lingua"}
+              </Card.Title>
             </Col>
           </Row>
-
-          <Card.Subtitle
-            className="mb-2 text-muted"
-            style={{ fontWeight: "400" }}
-          >
-            <EyeFill className="me-2" />
-            Solo per te
-          </Card.Subtitle>
         </Card.Body>
       </Card>
     </>
