946935e2f89ca4e8243bf21eca9b091233791880
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-02-28 13:10:36 +0100

    commit

--- src/Components/MyFooterPart.jsx
+++ src/Components/MyFooterPart.jsx
@@ -2,67 +2,68 @@ import { Container, Row, Col, Image, Form } from "react-bootstrap";
 
 const MyFooterPart = () => {
   return (
-    <Container style={{ backgroundColor: "red" }} id="footer" className="">
+    <Container id="footer" className="">
       <Row className="pl-3 pb-2 mt-5 footerRow">
         <Image src="../images/promoted/linkedinADS.png" alt="linkedin logo" />
       </Row>
+
       <Row className="footerRow">
         <Col xs={12} md={10}>
-          <Row>
+          <Row className="footerRow">
             <div className="mr-3">
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> Infromazioni </a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href="">Linee guida della community</a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href="">Privacy e condizioni</a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> sales solutions </a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href="">Centro sicurezza</a>
               </Col>
             </div>
             <div className="mr-3">
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> Accessibilità </a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> Carriera</a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> Opzioni di annuncio </a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href="">Mobile </a>
               </Col>
             </div>
             <div className="mr-3">
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> talent solutions </a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> Soluzioni di Marketing </a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> Pubblicità </a>
               </Col>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <a href=""> Piccole imprese </a>
               </Col>
             </div>
             <div id="footer-settings" className="mr-3">
-              <Col xs={12} md={6} lg={3} className="d-flex flex-row">
+              <Col className="d-flex flex-row">
                 <i class="bi bi-question-circle-fill mx-0 mt-2"></i>
                 <div className="ml-2">
                   <a href=""> Domande? </a>
                   <p> Visita il nostro centro assistenza </p>
                 </div>
               </Col>
-              <Col xs={12} md={6} lg={3} className="d-flex flex-row">
+              <Col className="d-flex flex-row">
                 <i class="bi bi-gear-fill mx-0 mt-2"></i>
                 <div className="ml-2">
                   <a href=""> Gestisci il tuo account e la tua privacy </a>
@@ -71,7 +72,7 @@ const MyFooterPart = () => {
               </Col>
             </div>
             <div>
-              <Col xs={12} md={6} lg={3}>
+              <Col>
                 <Form>
                   <Form.Group controlId="languages">
                     <Form.Label>Seleziona Lingua</Form.Label>
