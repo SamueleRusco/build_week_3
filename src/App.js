2ebc392b9a0e20e31c7bde87d9ffd96d180b8d3f
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-03-01 17:56:16 +0100

    commit menu impostation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import MyNavBarComponents from "./Components/MyNavBarComponents";
-import MyFooterPart from "./Components/MyFooterPart";
+import MyNavBarComponents from "./components/MyNavBarComponents";
+import Rightside from "./components/Rightside";
+import Main from "./components/Main";
+import Leftside from "./components/Leftside";
 
 
 function App() {
   return (
     <div className="App">
       <MyNavBarComponents/>
-      <MyFooterPart/>
+      <div className="body">
+  
+      <Leftside/>
+      <Main/>
+      
+      <Rightside/>
+      
+      </div>
     </div>
   );
 }
