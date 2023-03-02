3625b8e49eb2f21514fc4c3b107f99c19cf2d8ea
AleBellEpicode <114953308+AleBellEpicode@users.noreply.github.com>
2023-03-02 16:15:24 +0100

    commit

--- src/Components/MyEditPostComponent.jsx
+++ src/Components/MyEditPostComponent.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Button, Form, FormControl, FormLabel, Modal } from "react-bootstrap";
 import { PencilFill } from "react-bootstrap-icons";
+import { useDispatch } from "react-redux";
+import { putPostImg } from "../Redux/Actions/putProfileImg";
 
 const MyEditPostComponent = ({ editPost, setEditPost, postId }) => {
   const [text, setText] = useState("");
+  const [fd, setFd] = useState(new FormData());
   const url = "https://striveschool-api.herokuapp.com/api/posts/" + postId;
   const urlGet = "https://striveschool-api.herokuapp.com/api/posts/";
   console.log("sono post id", postId);
@@ -33,7 +36,14 @@ const MyEditPostComponent = ({ editPost, setEditPost, postId }) => {
     let data = await response.json();
     console.log("text", data);
   };
-
+  const handleFile = (ev) => {
+    setFd((prev) => {
+      //per cambiare i formData, bisogna "appendere" una nuova coppia chiave/valore, usando il metodo .append()
+      prev.delete("profile"); //ricordatevi di svuotare il FormData prima :)
+      prev.append("profile", ev.target.files[0]); //L'API richiede un "nome" diverso per ogni rotta, per caricare un'immagine ad un post, nel form data andra' inserito un valore con nome "post"
+      return prev;
+    });
+  };
   //   const getExperienceFetch = async () => {
   //     let key =
   //       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNjU5Y2YxOTNlNjAwMTM4MDdmNGQiLCJpYXQiOjE2Nzc0ODU0NzMsImV4cCI6MTY3ODY5NTA3M30.4UuEx0E0rg5moiQl2yjBzNkAo75xaKrDS6hY-r_GSLI";
