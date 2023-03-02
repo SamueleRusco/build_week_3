f83877a42c7953b8a643ec372e18f8b8841bba48
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-03-02 08:48:55 +0100

    commit

--- src/Components/MyMainComponent.jsx
+++ src/Components/MyMainComponent.jsx
@@ -11,16 +11,7 @@ const MyMainComponent = () => {
   return (
     <Container>
       <Row>
-        <Col xs={8} xl={7} className="offset-xl-1">
-          <MyUserDetailsCard />
-          <MySkillsQuizComponent />
-          <MyExperience />
-          <NewPost />
-          <Notizie />
-
-          <MyAnalisisComponent />
-          <MyForm />
-        </Col>
+        <Col xs={8} xl={7} className="offset-xl-1"></Col>
         <Col xs={4} xl={3} className="bg-danger "></Col>
       </Row>
     </Container>
