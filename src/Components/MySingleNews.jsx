b7f43e06363a98aa35a8fcd28cb53bc5daef9726
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-03-08 17:57:13 +0100

    commit

--- src/Components/MySingleNews.jsx
+++ src/Components/MySingleNews.jsx
@@ -38,106 +38,105 @@ const MySingleNews = ({
 
   return (
     <>
-      <Card className="my-2 py-3 text-start">
-        <Card.Body style={{ paddingTop: "0" }}>
-          <div className="d-flex justify-content-between">
-            <Card.Title style={{ fontSize: "1.2rem" }}>
-              <img
-                src={post.user.image}
-                alt="propic"
-                style={{
-                  width: "25px",
-                }}
-              />
-              {post.user.name} {post.user.surname}
-            </Card.Title>
-          </div>
-          <Card.Body style={{ position: "relative" }}>
-            <Row>
-              <Col xs={12}>
-                <p>{post.text}</p>
-                <div style={{}}></div>
-              </Col>
-            </Row>
-          </Card.Body>
-          <Card.Img
-            src={post.image}
-            style={{
-              width: "100%",
-              maxHeight: (post.image && "600px") || "0px",
-            }}
-            className="mb-3"
-          ></Card.Img>
-          <Card.Subtitle className=" text-muted" style={{ fontWeight: "400" }}>
-            pubblicato il {post?.createdAt?.substring(0, 10)}
-          </Card.Subtitle>
-          {(!selected && (
-            <Button
-              onClick={() => {
-                console.log(post._id);
-                // commenta(post._id);
-                // getCommentFetch();
-                // postCommentFetch();
-                setSelected(true);
+      <Card.Body style={{ paddingTop: "0" }}>
+        <div className="d-flex justify-content-between">
+          <Card.Title style={{ fontSize: "1.2rem" }}>
+            <img
+              src={post.user.image}
+              alt="propic"
+              style={{
+                width: "25px",
               }}
-            >
-              Scrivi commento
-            </Button>
-          )) || (
-            <div>
-              <Form>
-                <Row>
-                  <Col xs={7}>
-                    {" "}
-                    <Form.Control
-                      type="text"
-                      placeholder="Inserisci commento"
-                      value={commento}
-                      onChange={(e) => {
-                        setCommento(e.target.value);
-                      }}
-                    />
-                  </Col>
-                  <Col xs={2}>
-                    <Button
-                      onClick={() => {
-                        setRefreshed(true);
-                        postCommentFetch(commento, post._id);
-                        setSelected(false);
-                        setCommento("");
-                      }}
-                    >
-                      Invia
-                    </Button>
-                  </Col>
-                  <Col xs={3}>
-                    <Button
-                      className="bg-danger"
-                      onClick={() => {
-                        setSelected(false);
-                      }}
-                    >
-                      Annulla
-                    </Button>
-                  </Col>
-                </Row>
-              </Form>
-            </div>
-          )}
-          {post.user._id === profileID ? (
-            <>
-              <MyEditPostComponent
-                editPost={editPost}
-                setEditPost={setEditPost}
-                postId={post?._id}
-                setRefreshed={setRefreshed}
-                refreshed={refreshed}
-              />
-            </>
-          ) : (
-            ""
-          )}{" "}
-          {/* {rateComment &&
+            />
+            {post.user.name} {post.user.surname}
+          </Card.Title>
+        </div>
+        <Card.Body style={{ position: "relative" }}>
+          <Row>
+            <Col xs={12}>
+              <p>{post.text}</p>
+              <div style={{}}></div>
+            </Col>
+          </Row>
+        </Card.Body>
+        <Card.Img
+          src={post.image}
+          style={{
+            width: "100%",
+            maxHeight: (post.image && "600px") || "0px",
+          }}
+          className="mb-3"
+        ></Card.Img>
+        <Card.Subtitle className=" text-muted" style={{ fontWeight: "400" }}>
+          pubblicato il {post?.createdAt?.substring(0, 10)}
+        </Card.Subtitle>
+        {(!selected && (
+          <Button
+            onClick={() => {
+              console.log(post._id);
+              // commenta(post._id);
+              // getCommentFetch();
+              // postCommentFetch();
+              setSelected(true);
+            }}
+          >
+            Scrivi commento
+          </Button>
+        )) || (
+          <div>
+            <Form>
+              <Row>
+                <Col xs={7}>
+                  {" "}
+                  <Form.Control
+                    type="text"
+                    placeholder="Inserisci commento"
+                    value={commento}
+                    onChange={(e) => {
+                      setCommento(e.target.value);
+                    }}
+                  />
+                </Col>
+                <Col xs={2}>
+                  <Button
+                    onClick={() => {
+                      setRefreshed(true);
+                      postCommentFetch(commento, post._id);
+                      setSelected(false);
+                      setCommento("");
+                    }}
+                  >
+                    Invia
+                  </Button>
+                </Col>
+                <Col xs={3}>
+                  <Button
+                    className="bg-danger"
+                    onClick={() => {
+                      setSelected(false);
+                    }}
+                  >
+                    Annulla
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+        )}
+        {post.user._id === profileID ? (
+          <>
+            <MyEditPostComponent
+              editPost={editPost}
+              setEditPost={setEditPost}
+              postId={post?._id}
+              setRefreshed={setRefreshed}
+              refreshed={refreshed}
+            />
+          </>
+        ) : (
+          ""
+        )}{" "}
+        {/* {rateComment &&
             rateComment
               .filter((element) => element.elementId === post._id)
               .map((element, index) => {
@@ -173,8 +172,7 @@ const MySingleNews = ({
               Mostra meno
             </Button>
           )} */}
-        </Card.Body>
-      </Card>
+      </Card.Body>
     </>
   );
 };
