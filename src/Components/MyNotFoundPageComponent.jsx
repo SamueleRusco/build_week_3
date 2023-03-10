e300da8646371c12e0cbba6620a0aa5cf140ea63
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-03-10 11:48:23 +0100

    commit

--- src/Components/MyNotFoundPageComponent.jsx
+++ src/Components/MyNotFoundPageComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MyNotFoundPageComponent = () => {
   return (
-    <div style={{ height: "80vh" }}>
+    <div style={{ height: "100vh" }}>
       <div className="d-flex justify-content-center align-items-center h-100">
         <h1 className="text-secondary">404 not found</h1>
       </div>
