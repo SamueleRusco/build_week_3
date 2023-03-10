d63a64a576995ec1ca808f5e33b953523ec71455
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-03-10 12:27:31 +0100

    commit

--- src/Components/MyMainComponent.jsx
+++ src/Components/MyMainComponent.jsx
@@ -64,9 +64,9 @@ const MyMainComponent = () => {
           <MyPeopleCouldKnowComponent randomized={randomized} />
           {/* <MyGroupAdvisorComponent /> */}
           {/* <FavouriteJobsComponent /> */}
-          <Row className="py-3">
+          <Row className="py-3 mt-3">
             <Card.Title
-              className="mb-0"
+              className="mb-4 text-center"
               style={{ fontSize: "1.2rem", alignSelf: "center" }}
             >
               {"Lavori che ti interessano"}
@@ -74,8 +74,23 @@ const MyMainComponent = () => {
             {favouriteJobs &&
               favouriteJobs?.map((singleJob, index) => {
                 return (
-                  <div key={"ID: " + index} className="mb-2 mt-2">
-                    <Card>
+                  <div
+                    key={"ID: " + index}
+                    className={
+                      index === favouriteJobs?.length - 1 ? "mb-0" : "mb-2"
+                    }
+                  >
+                    <Card
+                      style={{
+                        border: "none",
+                        borderRadius:
+                          index === favouriteJobs?.length - 1 ? "8px" : "0px",
+                        borderBottom:
+                          index === favouriteJobs?.length - 1
+                            ? "none"
+                            : "1px solid lightgrey",
+                      }}
+                    >
                       <Card.Body>
                         <h5 style={{ fontSize: "1.1rem" }}>
                           <a
@@ -88,18 +103,21 @@ const MyMainComponent = () => {
                             {singleJob?.title}
                           </a>
                         </h5>
-                        <div className="d-flex">
-                          <p>
-                            {singleJob?.company_name}
-                            <span>·</span>
+                        <div>
+                          <p className="mb-1">{singleJob?.company_name}</p>
+                          <p className="mb-1">
+                            {singleJob?.candidate_required_location}
                           </p>
-                          <p>{singleJob?.candidate_required_location}</p>
-                          <span>·</span>
-                          <p>{singleJob?.publication_date?.substring(0, 10)}</p>
+
+                          <p className="mb-1">
+                            {singleJob?.publication_date?.substring(0, 10)}
+                          </p>
                         </div>
-                        <p>
+                        <p className="mb-1">
                           <BriefcaseFill className="me-2 mb-1 text-secondary" />
-                          {singleJob?.job_type}
+                          {singleJob?.job_type !== ""
+                            ? singleJob?.job_type
+                            : "Not specified"}
                         </p>
                         <div className="d-flex">
                           <div></div>
